c53ab187b530f678fa4f4cd33ada064ec7552c7a
ameba23 <ameba23@systemli.org>
2018-10-22 22:27:51 +0200

    added sql query

---
+++ sql_query.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3')
+// const filesize = require('filesize')
+
+var db = new sqlite3.Database('./database.sqlite3')
+var torrents = {}
+
+db.serialize(function () {
+  db.each('SELECT * FROM torrents', function (err, torrent) {
+    if (err) throw err
+    torrents[torrent.info_hash] = torrent
+    db.all(`SELECT size,path FROM files WHERE torrent_id=${torrent.id}`, function (err, files) {
+      if (err) throw err
+      torrents[torrent.info_hash].files = files
+      console.log(torrents[torrent.info_hash])
+    })
+  })
+})
+
